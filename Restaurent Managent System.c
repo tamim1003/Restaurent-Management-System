b9a3a18035fca2ceb068de6280aba72e3a81eaff
tamim1003 <106918778+tamim1003@users.noreply.github.com>
2022-12-06 23:53:36 +0600

    Update Restaurent Managent System.c

--- Restaurent Managent System.c
+++ Restaurent Managent System.c
@@ -31,3 +31,42 @@ int main_page()
     printf("      low cost..       *                                  *\n");
     printf("                       *                                  *\n");
     printf("     GRAB NOW !!       ");
+    
+     for (i=0;i<36;i++)
+     {
+        printf("*");
+     }
+    printf("\n\n\n\n                         Enter Your Choice: ");
+    int n;
+    scanf("%d",&n);
+    return n;
+}
+
+
+
+
+
+int menu_order()
+{
+
+    int arID[10],decision,arqty[10],Bill[10],counter=0;
+    work:
+    system("cls");
+    system("color F0");
+    printf("\n\t\t\t\tFOOD MENU\n\t\t\t\t---------\n");
+    printf("\n\tBUNS :\n\n");
+    printf("\t1. Beef Burger ...................... 120/=\t  \n");
+    printf("\t2. Hot Dog ..........................  40/=\n");
+    printf("\t3. Donuts ...........................  70/=\t \n");
+    printf("\t4. French Fries (FnF Pack) ..........  90/=\t \n");
+    printf("\t\t\t\t\t\t\t \n\n\tCHICKEN :\n\n");
+    printf("\t5. Chicken Breast ...................  90/=\n");
+    printf("\t6. Chicken Wings ....................  65/=\n");
+    printf("\n\tDESSERTS :\n\t\t\t\t\t\t\t \n");
+    printf("\t7. Ice Cream ........................  65/=\t  \n");
+    printf("\t8. Sweets ........................... 230/=\t \n");
+    printf("\n\tDRINKS :\n\n");
+    printf("\t9.  Coca-Cola ........................ 25/=\n");
+    printf("\t10. Mirinda   ........................ 25/=\n");
+    printf("\n\n\tOrder your dish: (Enter 0 to finish order)\n");
+    for(;;)
